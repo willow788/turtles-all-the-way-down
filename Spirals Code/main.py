a40a60c3f22fd356fa13a14435eff42972556d5c
SRIJA DE CHOWDHURY <70chaitalis@gmail.com>
2026-01-01 20:38:49 +0530

    Refactor spiral drawing code for clarity

--- Spirals Code/main.py
+++ Spirals Code/main.py
@@ -4,14 +4,24 @@ import cmath
 import turtle
 
 t = turtle.Turtle()
-t.speed(0)
-turtle.bgcolor("black")
-t.pensize(2)
-colors= ["#BA0505", "#A06903", "#CECE1D", "#0DB686", "#101076", "#41aa0d", "#D51DAD"]
+# Create a turtle object
 
+t.speed(0)  
+# Set the fastest drawing speed
+
+turtle.bgcolor("black") 
+# Set background color to black
+
+t.pensize(2) 
+# Set pen size
+
+colors= ["#BA0505", "#A06903", "#CECE1D", "#0DB686", "#101076", "#41aa0d", "#D51DAD"]  
+# Define a list of colors
+
+# Draw a colorful spiral
 for i in range(200):
-    t.color(colors[i % 7])
-    t.circle(i)
-    t.left(59)
+    t.color(colors[i % 7])   # Cycle through the colors
+    t.circle(i)              #draw the circle with radius i
+    t.left(59)               # Turn left by 59 degrees
 
-turtle.done()
+turtle.done() # Finish the drawing
