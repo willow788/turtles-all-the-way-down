3a895e660d7daf9ada9438b1d61d3de6fc80bade
SRIJA DE CHOWDHURY <70chaitalis@gmail.com>
2026-01-09 20:39:11 +0530

    Implement sunburst lines drawing with turtle graphics
    Added a turtle graphics program to draw sunburst lines from a circle edge.

--- Sunburst Lines from a circle edge/main.py
+++ Sunburst Lines from a circle edge/main.py
@@ -1,1 +1,32 @@
+import turtle
+import random
+import math
+
+t = turtle.Turtle()
+t.speed(0)
+t.hideturtle()
+#innitialised turtle pen
+
+#initializing the screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+t.color('gold')
+
+radious = 150
+lines = 120
+line_length = 300
+
+for i in range(lines):
+    angles  = 2 * math.pi / lines
+
+    x = radious * math.cos(angles * i)
+    y = radious * math.sin(angles * i)
+
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    t.setheading(math.degrees(angles * i))
+    t.forward(line_length)
+
+turtle.done()
 
