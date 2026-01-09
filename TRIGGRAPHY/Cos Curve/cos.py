92d7eeb095e3f2d27ef0c792cd34205c3f643968
SRIJA DE CHOWDHURY <70chaitalis@gmail.com>
2026-01-09 21:18:38 +0530

    Add cosine curve drawing using turtle graphics

---
+++ TRIGGRAPHY/Cos Curve/cos.py
@@ -0,0 +1,23 @@
+import turtle
+import random
+import math
+
+t = turtle.Turtle()
+t.speed(0)
+t.width(2)
+t.hideturtle()
+
+#screen initialization
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+t.penup()
+t.goto(-300, 0)
+t.pendown()
+t.color("hotpink")
+
+for i in range(-300, 300):
+    y = 80 * math.cos(math.radians(i))
+    t.goto(i, y)
+
+turtle.done()
